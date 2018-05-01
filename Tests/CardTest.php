379dcbfe37f801a1b7f7196d3580f566f5546434
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-01 14:31:44 +0100

    changed testGetSuit and testGetValue to fail before upcoming changes to those functions in Card.php

--- Tests/CardTest.php
+++ Tests/CardTest.php
@@ -1,6 +1,5 @@
 <?php
 
-require 'Card.php';
 
 class CardTest extends PHPUnit\Framework\TestCase
 {
@@ -17,35 +16,16 @@ class CardTest extends PHPUnit\Framework\TestCase
 
     public function testGetSuit()
     {
-        $card = new Card(suit, value);
-        $expected = [
-            1 => "Diamond",
-            2 => "Club",
-            3 => "Heart",
-            4 => "Spade"
-        ];
+        $card = new Card("Heart", 12);
+        $expected = "Heart";
 
         $this->assertEquals($expected, $card->getSuit());
     }
 
     public function testValue()
     {
-        $card = new Card(suit, value);
-        $expected = [
-            "A" => 1,
-            "2" => 2,
-            "3" => 3,
-            "4" => 4,
-            "5" => 5,
-            "6" => 6,
-            "7" => 7,
-            "8" => 8,
-            "9" => 9,
-            "10" => 10,
-            "J" => 11,
-            "Q" => 12,
-            "K" => 13
-        ];
+        $card = new Card("Heart", 12);
+        $expected = "12";
 
         $this->assertEquals($expected, $card->getValue());
     }
