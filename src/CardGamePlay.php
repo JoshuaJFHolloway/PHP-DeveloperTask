7eba1142332e83999d3babc93799115b312e5c41
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-08 11:12:56 +0100

    added code for showing players' hands before cards dealt to them

--- src/CardGamePlay.php
+++ src/CardGamePlay.php
@@ -1,8 +1,13 @@
 <?php
 
 require 'Deck.php';
+require 'Player.php';
 
 $deck = new Deck();
+$player1 = new Player("John");
+$player2 = new Player("Mary");
+$player3 = new Player("Tim");
+$player4 = new Player("Lucy");
 
 echo("\n");
 
@@ -45,5 +50,13 @@ echo("\n");
 
 echo "First card in the shuffled deck:"."\n";
 echo $deck['0'] . "\n";
+
+echo "Dealing cards to all 4 players"."\n";
+
+echo $player1;
+echo $player2;
+echo $player3;
+echo $player4;
+
 
 echo("\n");
